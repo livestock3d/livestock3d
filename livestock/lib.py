64afe97b1bf795bbd79c201688992dea6b54b620
ocni-dtu <ocni@dtu.dk>
2018-02-13 19:01:49 +0100

    bug fixes

--- livestock/lib.py
+++ livestock/lib.py
@@ -17,12 +17,14 @@ __license__ = "MIT"
 def my_function(folder):
 
     file = open(folder + '/my_file.txt', 'r')
-    my_lines = file.readlines()
+    my_lines = [line.strip()
+                for line in file.readlines()]
+
     file.close()
     print(my_lines)
 
     result_file = open(folder + '/my_result.txt', 'w')
-    result_file.write(my_lines)
+    [result_file.write(line) for line in my_lines]
     result_file.close()
 
     return None
